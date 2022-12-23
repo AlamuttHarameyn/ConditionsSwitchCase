4fd9e9e1993af6192796f844305e08c57fc3b9cf
Gizem <gizemustahuseyin123@gmail.com>
2022-12-23 21:11:48 +0300

    Add files via upload

---
+++ SwitchCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class SwitchCase
+    {
+        static void Main(string[] args)
+        {
+            var number = 20;
+            switch (number)//switch için şart yazıyoruz
+            {
+                case 10://10 olma  durumunda ekrana yazdır
+                    Console.WriteLine("Number is 10");
+                break;//
+
+                case 20://20 olma durumunda ekrana yazdır
+                    Console.WriteLine("Number is 20");
+                    break;
+
+                default: //iki şartı da sağlamıyorsa ekrana yazdır
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
+                    
+                    
+            }
+            Console.ReadKey();
+        }
+    }
+}
